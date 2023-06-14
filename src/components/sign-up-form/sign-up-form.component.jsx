6a42e2fb215e6e089ced7afee211254c434e917b
JosephLee19 <leejos17@gmail.com>
2023-06-14 14:09:26 -0400

    created authentication page component alongside sign in form

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -95,9 +95,7 @@ const SignUpForm = () => {
           name="confirmPassword"
           value={confirmPassword}
         />
-        <Button buttonType="inverted" type="submit">
-          Sign Up
-        </Button>
+        <Button type="submit">Sign Up</Button>
       </form>
     </div>
   );
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,44 +1,48 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, GithubAuthProvider, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCAU1a6YzCCR5P7AoKzti_GxT_BE6SrLeE",
-    authDomain: "mrthn-clothing-db.firebaseapp.com",
-    projectId: "mrthn-clothing-db",
-    storageBucket: "mrthn-clothing-db.appspot.com",
-    messagingSenderId: "590943243635",
-    appId: "1:590943243635:web:c422622839724602f78906"
-  };
-  
-  // Initialize Firebase
+  apiKey: "AIzaSyCAU1a6YzCCR5P7AoKzti_GxT_BE6SrLeE",
+  authDomain: "mrthn-clothing-db.firebaseapp.com",
+  projectId: "mrthn-clothing-db",
+  storageBucket: "mrthn-clothing-db.appspot.com",
+  messagingSenderId: "590943243635",
+  appId: "1:590943243635:web:c422622839724602f78906",
+};
+
+// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
 const googleProvider = new GoogleAuthProvider();
-const provider2 = new GithubAuthProvider();
 
 googleProvider.setCustomParameters({
-  prompt: "select_account"
-});
-
-provider2.setCustomParameters({
-  prompt: "select_account"
+  prompt: "select_account",
 });
 
 export const auth = getAuth();
 
-export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
-export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
-
-export const signInWithGithubPopup = () => signInWithPopup(auth, provider2);
-export const signInWithGithubRedirect = () => signInWithRedirect(auth, provider2);
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
   if (!userAuth) return;
 
-  const userDocRef = doc(db, 'users', userAuth.uid);
+  const userDocRef = doc(db, "users", userAuth.uid);
   const userSnapshot = await getDoc(userDocRef);
 
   if (!userSnapshot.exists()) {
@@ -46,18 +50,27 @@ export const createUserDocumentFromAuth = async (userAuth, additionalInformation
     const createdAt = new Date();
     try {
       await setDoc(userDocRef, {
-        displayName, email, createdAt, ...additionalInformation
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
       });
     } catch (error) {
-      console.log('error creating user ', error.message)
+      console.log("error creating user ", error.message);
     }
-  } 
+  }
 
   return userDocRef;
-}
+};
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
   if (!email || !password) return;
 
   return createUserWithEmailAndPassword(auth, email, password);
-}
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return signInWithEmailAndPassword(auth, email, password);
+};
