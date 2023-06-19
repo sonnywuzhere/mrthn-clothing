9bacfdc9095843f3cbc5015c0a38bc8f89ff23ea
JosephLee19 <leejos17@gmail.com>
2023-06-19 15:18:13 -0400

    add in addToCart feature

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,22 @@
+import { useContext } from "react";
+
+import { CartContext } from "../../contexts/cart-contextx";
+
+import CartItem from "../cart-item/cart-item.component";
 import Button from "../button/button.component";
 
 import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        <Button buttonType="inverted">GO TO CHECKOUT</Button>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
       </div>
+      <Button>GO TO CHECKOUT</Button>
     </div>
   );
 };
